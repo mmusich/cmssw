25984965dd09c954a70a7eb6f7c9f134e32d1192
Marco Musich <marco.musich@cern.ch>
2014-05-13 12:54:53 +0200

    changing wrong input file name

--- SLHCUpgradeSimulations/Geometry/python/fakeConditions_Phase1SmallPixel_cff.py
+++ SLHCUpgradeSimulations/Geometry/python/fakeConditions_Phase1SmallPixel_cff.py
@@ -2,11 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 siPixelFakeGainOfflineESSource = cms.ESSource("SiPixelFakeGainOfflineESSource",
                                               file = cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseI/EmptyPixelSkimmedGeometry.txt')
+                                              # should we use this one instead?
+                                              # file = cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseI/PixelSkimmedGeometry_phase1.txt')
                                               )
 es_prefer_fake_gain = cms.ESPrefer("SiPixelFakeGainOfflineESSource","siPixelFakeGainOfflineESSource")
 
 siPixelFakeLorentzAngleESSource = cms.ESSource("SiPixelFakeLorentzAngleESSource",
-                                               file = cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseI/PixelSkimmedGeometry.txt')
+                                               file = cms.FileInPath('SLHCUpgradeSimulations/Geometry/data/PhaseI/PixelSkimmedGeometry_phase1.txt')
                                                )
 
 es_prefer_fake_lorentz = cms.ESPrefer("SiPixelFakeLorentzAngleESSource","siPixelFakeLorentzAngleESSource")
