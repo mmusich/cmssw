64601dd4b7b37f18741d3bf9af41b9dd33a831c2
mmusich <marco.musich@cern.ch>
2015-06-26 12:14:46 +0200

    Fixing the matrix for 50ns PDs

--- Configuration/AlCa/python/autoAlca.py
+++ Configuration/AlCa/python/autoAlca.py
@@ -1,23 +1,26 @@
-AlCaRecoMatrix = {'ExpressCosmics' : 'SiStripCalZeroBias+TkAlCosmics0T',
-                  'StreamExpress'  : 'SiStripCalZeroBias+TkAlMinBias+MuAlCalIsolatedMu+DtCalib',
-                  'MinimumBias'    : 'SiStripCalMinBias+TkAlMinBias',
-                  'Commissioning'  : 'HcalCalIsoTrk',
-                  'SingleMu'       : 'MuAlCalIsolatedMu+MuAlOverlaps+TkAlMuonIsolated+DtCalib',
-                  'DoubleMu'       : 'MuAlCalIsolatedMu+MuAlOverlaps+DtCalib+TkAlZMuMu',
-                  'MuOnia'         : 'TkAlJpsiMuMu+TkAlUpsilonMuMu',
-                  'SingleElectron' : 'EcalCalWElectron+EcalUncalWElectron',
-                  'DoubleElectron' : 'EcalCalZElectron+EcalUncalZElectron',
-                  'AlCaLumiPixels' : 'LumiPixels',
-                  'DoubleMuParked' : 'MuAlCalIsolatedMu+MuAlOverlaps+DtCalib+TkAlZMuMu',
-                  'MuOniaParked'   : 'TkAlJpsiMuMu+TkAlUpsilonMuMu',
-                  'Cosmics'        : 'TkAlCosmics0T+MuAlGlobalCosmics+HcalCalHOCosmics+DtCalibCosmics',
-                  # These two cannot run on RAW, they are just meant to run on the dedicated AlcaRAW so they do not enter the allForPrompt list
-                  'AlCaP0'         : '',
-                  # ---------------------------------------------------------------------------------------------------------------------------
-                  'HcalNZS'        : 'HcalCalMinBias'
+AlCaRecoMatrix ={'ExpressCosmics' : 'SiStripCalZeroBias+TkAlCosmics0T',
+                 'StreamExpress'  : 'SiStripPCLHistos+SiStripCalZeroBias+SiStripCalMinBias+TkAlMinBias',
+                 'Commissioning'  : 'HcalCalIsoTrk',
+                 'Cosmics'        : 'TkAlCosmics0T+MuAlGlobalCosmics+HcalCalHOCosmics+DtCalibCosmics',
+                 'SingleMu'       : 'TkAlMuonIsolated+DtCalib+MuAlCalIsolatedMu+MuAlOverlaps+MuAlZMuMu',
+                 'DoubleMu'       : 'MuAlCalIsolatedMu+MuAlOverlaps+MuAlZMuMu+TkAlZMuMu',
+                 'MuOnia'         : 'TkAlUpsilonMuMu',
+                 'Charmonium'     : 'TkAlJpsiMuMu',
+                 'MET'            : 'HcalCalNoise',
+                 'JetHT'          : 'HcalCalDijets',
+                 'SinglePhoton'   : 'HcalCalGammaJet',
+                 'SingleElectron' : 'EcalCalWElectron+EcalUncalWElectron, EcalCalZElectron, EcalUncalZElectron',
+                 'DoubleEG'       : 'EcalCalZElectron+EcalUncalZElectron',
+                 'ZeroBias'       : 'SiStripCalZeroBias+TkAlMinBias+LumiPixelsMinBias',
+                 'HcalNZS'        : 'HcalCalMinBias+HcalCalIterativePhiSym',
+                 'HLTPhysics'     : 'SiStripCalMinBias+TkAlMinBias',
+                 'AlCaLumiPixels' : 'LumiPixels',
+                 'TestEnabledEcalHcal' : 'HcalCalMinBias',
+                  # --------------------------------------------------------------------------------------------------------------------------
+                  'HcalNZS'        : 'HcalCalMinBias',
                   # This is in the AlCaRecoMatrix, but no RelVals are produced
                   # 'TestEnablesTracker' : 'TkAlLAS'
-                  }
+                 } 
 
 def buildList(pdList, matrix):
     """Takes a list of primary datasets (PDs) and the AlCaRecoMatrix (a dictinary) and returns a string with all the AlCaRecos for the selected PDs separated by the '+' character without duplicates."""
@@ -35,7 +38,7 @@ def buildList(pdList, matrix):
     return stringList
 
 # Update the lists anytime a new PD is added to the matrix
-autoAlca = { 'allForPrompt'         : buildList(['MinimumBias', 'Commissioning', 'SingleMu', 'DoubleMu', 'MuOnia', 'DoubleMuParked', 'MuOniaParked', 'SingleElectron', 'DoubleElectron', 'HcalNZS'], AlCaRecoMatrix),
+autoAlca = { 'allForPrompt'         : buildList(['Commissioning', 'SingleMu', 'DoubleMu', 'MuOnia', 'Charmonium', 'MET', 'JetHT', 'SingleElectron', 'SinglePhoton', 'DoubleEG', 'ZeroBias' , 'HLTPhysics', 'AlCaLumiPixels', 'HcalNZS'], AlCaRecoMatrix),
              'allForExpress'        : buildList(['StreamExpress'], AlCaRecoMatrix),
              'allForPromptCosmics'  : buildList(['Cosmics'], AlCaRecoMatrix),
              'allForExpressCosmics' : buildList(['ExpressCosmics'], AlCaRecoMatrix) }
