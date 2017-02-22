cbf4a5d456455925b6efc702475bc4784a082f05
mmusich <marco.musich@cern.ch>
2017-02-22 16:15:07 +0100

    add the significance cut to the members of the AlignPCLThreshold class

--- CondFormats/PCLConfig/src/AlignPCLThreshold.cc
+++ CondFormats/PCLConfig/src/AlignPCLThreshold.cc
@@ -1,18 +1,19 @@
 #include "CondFormats/PCLConfig/interface/AlignPCLThreshold.h"
 
- AlignPCLThreshold:: AlignPCLThreshold(double Xcut,double tXcut, 
+AlignPCLThreshold:: AlignPCLThreshold(double sigCut,
+				       double Xcut,double tXcut, 
 				       double Ycut,double tYcut, 
 				       double Zcut,double tZcut, 
 				       double maxMoveCut,double maxErrorCut 
 				       ){
-   
-   m_Xcut        = Xcut;              
-   m_tXcut       = tXcut;      
-   m_Ycut        = Ycut;       
-   m_tYcut       = tYcut;      
-   m_Zcut        = Zcut;       
-   m_tZcut       = tZcut;      
-   m_maxMoveCut  = maxMoveCut; 
-   m_maxErrorCut = maxErrorCut;
+  m_sigCut      = sigCut;
+  m_Xcut        = Xcut;              
+  m_tXcut       = tXcut;      
+  m_Ycut        = Ycut;       
+  m_tYcut       = tYcut;      
+  m_Zcut        = Zcut;       
+  m_tZcut       = tZcut;      
+  m_maxMoveCut  = maxMoveCut; 
+  m_maxErrorCut = maxErrorCut;
 
 };
