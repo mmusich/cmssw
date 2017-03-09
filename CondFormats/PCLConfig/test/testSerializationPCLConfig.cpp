73ff68f6bcfd56c30e933d1a65f44b7b9edf5145
mmusich <marco.musich@cern.ch>
2017-03-09 16:18:53 +0100

    Fix and extend serialization tests

--- CondFormats/PCLConfig/test/testSerializationPCLConfig.cpp
+++ CondFormats/PCLConfig/test/testSerializationPCLConfig.cpp
@@ -5,5 +5,7 @@
 int main()
 {
   testSerialization<AlignPCLThresholds>();
+  testSerialization<std::vector<AlignPCLThreshold>>();
+  testSerialization<std::vector<AlignPCLThreshold::coordThresholds>>();
   return 0;
 }
