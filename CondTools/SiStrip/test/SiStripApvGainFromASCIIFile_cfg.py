c700a9eed5c052a45e41d8bd3e297843863afff1
mmusich <marco.musich@cern.ch>
2017-03-31 16:55:04 +0200

    Modernize configuration and remove huge example file

--- CondTools/SiStrip/test/SiStripApvGainFromASCIIFile_cfg.py
+++ CondTools/SiStrip/test/SiStripApvGainFromASCIIFile_cfg.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-
 process = cms.Process("ICALIB")
 process.source = cms.Source("EmptyIOVSource",
     firstValue = cms.uint64(235679),
@@ -8,8 +7,6 @@ process.source = cms.Source("EmptyIOVSource",
     timetype = cms.string('runnumber'),
     interval = cms.uint64(1)
 )
-
-
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.threshold = cms.untracked.string('DEBUG')
@@ -22,37 +19,33 @@ process.MessageLogger.cout = cms.untracked.PSet( threshold = cms.untracked.strin
 process.load('Configuration.Geometry.GeometryIdeal_cff')
 
 #Setup the SiSTripFedCabling and the SiStripDetCabling
-process.load("CondCore.DBCommon.CondDBCommon_cfi")
-process.CondDBCommon.connect='frontier://FrontierProd/CMS_COND_31X_STRIP'
-
-process.poolDBESSource = cms.ESSource( 'PoolDBESSource',
-                                       process.CondDBCommon,
-                                       BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
-                                       toGet = cms.VPSet( 
-                                                          cms.PSet( record = cms.string('SiStripFedCablingRcd'),
-                                                                    tag    = cms.string('SiStripFedCabling_GR10_v1_hlt')
-                                                                  )
-                                                        )
-                                     )
+from CondCore.CondDB.CondDB_cfi import *
+CondDBDetVOff = CondDB.clone(connect = cms.string('frontier://FrontierProd/CMS_CONDITIONS'))
+process.dbInput = cms.ESSource("PoolDBESSource",
+                               CondDBDetVOff,
+                               toGet = cms.VPSet(cms.PSet(record = cms.string('SiStripFedCablingRcd'),
+                                                          tag    = cms.string('SiStripFedCabling_GR10_v1_hlt')
+                                                          )
+                                                 )
+                               )
                                                                     
 process.load("CalibTracker.SiStripESProducers.SiStripConnectivity_cfi")
 
-
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
-)
+    )
+
 process.PoolDBOutputService = cms.Service("PoolDBOutputService",
-    BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
-    DBParameters = cms.PSet(
-        authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
-    ),
-    timetype = cms.untracked.string('runnumber'),
-    connect = cms.string('sqlite_file:dbfile.db'),
-    toPut = cms.VPSet(cms.PSet(
-        record = cms.string('SiStripApvGainRcd'),
-        tag = cms.string('SiStripApvGainRcd_v1')
-    ))
-)
+                                          BlobStreamerName = cms.untracked.string('TBufferBlobStreamingService'),
+                                          DBParameters = cms.PSet(authenticationPath = cms.untracked.string('/afs/cern.ch/cms/DB/conddb')
+                                                                  ),
+                                          timetype = cms.untracked.string('runnumber'),
+                                          connect = cms.string('sqlite_file:dbfile.db'),
+                                          toPut = cms.VPSet(cms.PSet(record = cms.string('SiStripApvGainRcd'),
+                                                                     tag = cms.string('SiStripApvGainRcd_v1')
+                                                                     )
+                                                            )
+                                          )
 
 process.prod = cms.EDAnalyzer("SiStripApvGainFromFileBuilder",
     outputMaps    = cms.untracked.bool(True),
