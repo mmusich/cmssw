903394bcdfb859ac3620853ba88573aa828a7563
mmusich <marco.musich@cern.ch>
2017-05-09 13:15:13 +0200

    Switch-on luminosity info production in hit efficiency tree

--- CalibTracker/Configuration/python/setupCalibrationTree_cff.py
+++ CalibTracker/Configuration/python/setupCalibrationTree_cff.py
@@ -11,6 +11,7 @@ shallowClusters.Clusters                = 'CalibrationTracks'
 shallowGainCalibration.Tracks           = 'CalibrationTracksRefit'
 anEff.combinatorialTracks               = 'CalibrationTracksRefit'
 anEff.trajectories                      = 'CalibrationTracksRefit'
+anEff.addLumi                           = True
 
 #Schedule
 #TkCalFullSequence = cms.Sequence( trackFilterRefit + LorentzAngleNtuple + hiteff + OfflineGainNtuple)
@@ -20,5 +21,4 @@ TkCalSeq_IsoMuon   = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + O
 TkCalSeq_IsoMuon0T = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_IsoMuon0T + hiteff)
 TkCalSeq_AagBunch   = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_AagBunch + hiteff)
 TkCalSeq_AagBunch0T = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_AagBunch0T + hiteff)
-
-
+TkCalSeq_HitEffOnly = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + hiteff)
--- CalibTracker/SiStripCommon/test/MakeCalibrationTrees/produceCalibrationTree_template_cfg.py
+++ CalibTracker/SiStripCommon/test/MakeCalibrationTrees/produceCalibrationTree_template_cfg.py
@@ -31,12 +31,8 @@ options.register('outputFile',
                  "name for the output root file (\"calibTreeTest.root\" is default)")
 
 options.register('inputFiles',
-                 "/store/data/Run2015D/ZeroBias/ALCARECO/SiStripCalMinBias-16Dec2015-v1/60007/869EE593-1FAB-E511-AF99-0025905A60B4.root",
-#                  '/store/data/Run2015D/ZeroBias/ALCARECO/SiStripCalMinBias-16Dec2015-v1/60009/0C35C6BF-D3AA-E511-9BC9-0CC47A4C8E16.root',
-#                  '/store/data/Run2015D/ZeroBias/ALCARECO/SiStripCalMinBias-16Dec2015-v1/60009/38B847F9-05AA-E511-AB78-00259074AE82.root',
-#                  '/store/data/Run2015D/ZeroBias/ALCARECO/SiStripCalMinBias-16Dec2015-v1/60009/D0BAD20B-09AB-E511-B073-0026189438F6.root',
-#                  '/store/data/Run2015D/ZeroBias/ALCARECO/SiStripCalMinBias-16Dec2015-v1/60009/DEFA8704-CCAA-E511-8203-0CC47A4D7634.root',
-#                  '/store/data/Run2015D/ZeroBias/ALCARECO/SiStripCalMinBias-16Dec2015-v1/60009/FE24690A-2DAA-E511-A96A-00259074AE3E.root',
+                 "/store/data/Run2016H/ZeroBias/ALCARECO/SiStripCalMinBias-18Apr2017-v1/60001/F89C5EEF-2F27-E711-9095-0025905AA9F0.root",   ## ALCARECO with luminosity info
+                 #"/store/data/Run2015D/ZeroBias/ALCARECO/SiStripCalMinBias-16Dec2015-v1/60007/869EE593-1FAB-E511-AF99-0025905A60B4.root",  ## ALCARECO without luminosity info
                  VarParsing.VarParsing.multiplicity.list,
                  VarParsing.VarParsing.varType.string,
                  "file to process")
@@ -63,8 +59,6 @@ print "inputCollection   : ", options.inputCollection
 print "maxEvents         : ", options.maxEvents
 print "outputFile        : ", options.outputFile
 print "inputFiles        : ", options.inputFiles
-
-
 
 process = cms.Process('CALIB')
 process.load('CalibTracker.Configuration.setupCalibrationTree_cff')
@@ -113,6 +107,7 @@ process.shallowTracks.Tracks  = 'ALCARECOSiStripCalMinBias' #cms.InputTag( optio
 ## process.L1T1.L1SeedsLogicalExpression = cms.string('(40 OR 41) AND NOT (36 OR 37 OR 38 OR 39)')
 
 #process.TkCalPath = cms.Path(process.L1T1*process.TkCalFullSequence)
+#process.TkCalPath_HitEffOnly = cms.Path(process.TkCalSeq_HitEffOnly)
 process.TkCalPath_StdBunch   = cms.Path(process.TkCalSeq_StdBunch)
 process.TkCalPath_StdBunch0T = cms.Path(process.TkCalSeq_StdBunch0T)
 process.TkCalPath_IsoMuon    = cms.Path(process.TkCalSeq_IsoMuon)
@@ -120,15 +115,14 @@ process.TkCalPath_IsoMuon0T  = cms.Path(process.TkCalSeq_IsoMuon0T)
 process.TkCalPath_AagBunch   = cms.Path(process.TkCalSeq_AagBunch)
 process.TkCalPath_AagBunch0T = cms.Path(process.TkCalSeq_AagBunch0T)
 
-
-process.schedule = cms.Schedule( process.TkCalPath_StdBunch, 
-                                 process.TkCalPath_StdBunch0T,
-                                 process.TkCalPath_IsoMuon,
-                                 process.TkCalPath_IsoMuon0T,
-                                 process.TkCalPath_AagBunch,
-                                 process.TkCalPath_AagBunch0T,
-                               )
-
+process.schedule = cms.Schedule(process.TkCalPath_StdBunch, 
+                                process.TkCalPath_StdBunch0T,
+                                process.TkCalPath_IsoMuon,
+                                process.TkCalPath_IsoMuon0T,
+                                process.TkCalPath_AagBunch,
+                                process.TkCalPath_AagBunch0T,
+                                #process.TkCalPath_HitEffOnly
+                                )
 
 process.options = cms.untracked.PSet(
     Rethrow = cms.untracked.vstring('OtherCMS', 
