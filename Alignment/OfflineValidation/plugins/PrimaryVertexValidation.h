df6e110d2d328766579dab14cc5b000781d89392
mmusich <marco.musich@cern.ch>
2017-07-10 13:04:58 +0200

    towards modularity

--- Alignment/OfflineValidation/plugins/PrimaryVertexValidation.h
+++ Alignment/OfflineValidation/plugins/PrimaryVertexValidation.h
@@ -59,13 +59,43 @@
 // residuals moments estimation 
 //
 
-namespace statmode{
+namespace pvparams{
+  
   enum estimator 
     { MEAN   = 1,
       WIDTH  = 2, 
       MEDIAN = 3,
       MAD    = 4,
       UNKWN  = -1
+    };
+  
+  enum residualType 
+    { dxy = 1,
+      dx  = 2,
+      dy  = 3,
+      dz  = 4,
+      IP2D = 5,
+      resz = 6,
+      IP3D = 7,
+      d3D  = 8,
+      
+      norm_dxy = 9,
+      norm_dx  = 10,
+      norm_dy  = 11,
+      norm_dz  = 12,
+      norm_IP2D = 13,
+      norm_resz = 14,
+      norm_IP3D = 15,
+      norm_d3D  = 16,
+    };
+    
+  enum plotVariable
+    { phi    = 1,
+      eta    = 2,
+      pT     = 3,
+      pTCentral = 4,
+      ladder = 5,
+      modZ   = 6,
     };
 }
 
@@ -90,13 +120,14 @@ class PrimaryVertexValidation : public edm::one::EDAnalyzer<edm::one::SharedReso
   Measurement1D getMedian(TH1F *histo);
   Measurement1D getMAD(TH1F *histo);
   std::pair<Measurement1D, Measurement1D > fitResiduals(TH1 *hist);
-  void fillTrendPlot(TH1F* trendPlot, TH1F *residualsPlot[100], statmode::estimator fitPar_, TString var_);
-  void fillTrendPlotByIndex(TH1F* trendPlot,std::vector<TH1F*>& h, statmode::estimator fitPar_); 
+  void fillTrendPlot(TH1F* trendPlot, TH1F *residualsPlot[100], pvparams::estimator fitPar_, TString var_);
+  void fillTrendPlotByIndex(TH1F* trendPlot,std::vector<TH1F*>& h, pvparams::estimator fitPar_); 
 
   static bool vtxSort( const reco::Vertex &  a, const reco::Vertex & b );
   bool passesTrackCuts(const reco::Track & track, const reco::Vertex & vertex,std::string qualityString_, double dxyErrMax_,double dzErrMax_, double ptErrMax_);
 
-  std::vector<TH1F*> bookResidualsHistogram(TFileDirectory dir,unsigned int theNOfBins,TString resType,TString varType); 
+  std::vector<TH1F*> bookResidualsHistogram(TFileDirectory dir,unsigned int theNOfBins,pvparams::residualType resType,pvparams::plotVariable varType, bool isNormalized=false); 
+
   std::map<std::string, TH1*> bookVertexHistograms(TFileDirectory dir);
   void fillTrackHistos(std::map<std::string, TH1*> & h, const std::string & ttype, const reco::TransientTrack *tt, const reco::Vertex & v,const reco::BeamSpot & beamSpot, double fBfield);
   void add(std::map<std::string, TH1*>& h, TH1* hist);
@@ -104,7 +135,10 @@ class PrimaryVertexValidation : public edm::one::EDAnalyzer<edm::one::SharedReso
   void fill(std::map<std::string, TH1*>& h, std::string s, double x, double y);
   void fillByIndex(std::vector<TH1F*>& h, unsigned int index, double x); 
   void shrinkHistVectorToFit(std::vector<TH1F*>&h,unsigned int desired_size);
-  void fillMap(TH2F* trendMap, TH1F* residualsMapPlot[100][100], statmode::estimator fitPar_);
+  std::string getTypeString (pvparams::residualType type);
+  std::string getVarString (pvparams::plotVariable var);
+
+  void fillMap(TH2F* trendMap, TH1F* residualsMapPlot[100][100], pvparams::estimator fitPar_);
   
   inline double square(double x){
     return x*x;
