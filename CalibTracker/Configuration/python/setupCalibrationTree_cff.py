5958a0aa6bf931eea008b08d2812270f5e8dd7b4
mmusich <marco.musich@cern.ch>
2017-10-18 13:29:38 +0200

    add the hit resolution analyzer in the calibration tree

--- CalibTracker/Configuration/python/setupCalibrationTree_cff.py
+++ CalibTracker/Configuration/python/setupCalibrationTree_cff.py
@@ -4,6 +4,7 @@ from CalibTracker.Configuration.Filter_Refit_cff import *
 from CalibTracker.SiStripLorentzAngle.ntuple_cff import *
 from CalibTracker.SiStripChannelGain.ntuple_cff import *
 from CalibTracker.SiStripHitEfficiency.SiStripHitEff_cff import *
+from CalibTracker.SiStripHitResolution.SiStripHitResolution_cff import *
 
 shallowTrackClusters.Tracks             = 'CalibrationTracksRefit'
 shallowTrackClusters.Clusters           = 'CalibrationTracks'
@@ -11,14 +12,16 @@ shallowClusters.Clusters                = 'CalibrationTracks'
 shallowGainCalibration.Tracks           = 'CalibrationTracksRefit'
 anEff.combinatorialTracks               = 'CalibrationTracksRefit'
 anEff.trajectories                      = 'CalibrationTracksRefit'
+anRes.tracks                            = 'CalibrationTracksRefit' 
+anRes.trajectories                      = 'CalibrationTracksRefit'
 
 #Schedule
 #TkCalFullSequence = cms.Sequence( trackFilterRefit + LorentzAngleNtuple + hiteff + OfflineGainNtuple)
-TkCalSeq_StdBunch   = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_StdBunch + hiteff)
-TkCalSeq_StdBunch0T = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_StdBunch0T + hiteff)
-TkCalSeq_IsoMuon   = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_IsoMuon + hiteff)
-TkCalSeq_IsoMuon0T = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_IsoMuon0T + hiteff)
-TkCalSeq_AagBunch   = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_AagBunch + hiteff)
-TkCalSeq_AagBunch0T = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_AagBunch0T + hiteff)
+TkCalSeq_StdBunch   = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_StdBunch + hiteff + hitres)
+TkCalSeq_StdBunch0T = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_StdBunch0T + hiteff + hitres)
+TkCalSeq_IsoMuon    = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_IsoMuon + hiteff + hitres)
+TkCalSeq_IsoMuon0T  = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_IsoMuon0T + hiteff + hitres)
+TkCalSeq_AagBunch   = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_AagBunch + hiteff + hitres)
+TkCalSeq_AagBunch0T = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_AagBunch0T + hiteff + hitres)
 
 
