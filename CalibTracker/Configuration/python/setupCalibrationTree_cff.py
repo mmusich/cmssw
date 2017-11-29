e2b877fa5200f5343d03cda1cd7d81c3f3de73ed
mmusich <marco.musich@cern.ch>
2017-11-29 18:14:04 +0100

    add event prescaler to calibTrees

--- CalibTracker/Configuration/python/setupCalibrationTree_cff.py
+++ CalibTracker/Configuration/python/setupCalibrationTree_cff.py
@@ -4,6 +4,7 @@ from CalibTracker.Configuration.Filter_Refit_cff import *
 from CalibTracker.SiStripLorentzAngle.ntuple_cff import *
 from CalibTracker.SiStripChannelGain.ntuple_cff import *
 from CalibTracker.SiStripHitEfficiency.SiStripHitEff_cff import *
+from CalibTracker.SiStripCommon.prescaleEvent_cfi import *
 
 shallowTrackClusters.Tracks             = 'CalibrationTracksRefit'
 shallowTrackClusters.Clusters           = 'CalibrationTracks'
@@ -11,6 +12,8 @@ shallowClusters.Clusters                = 'CalibrationTracks'
 shallowGainCalibration.Tracks           = 'CalibrationTracksRefit'
 anEff.combinatorialTracks               = 'CalibrationTracksRefit'
 anEff.trajectories                      = 'CalibrationTracksRefit'
+prescaleEvent.prescale                  = 1
+
 
 #Schedule
 #TkCalFullSequence = cms.Sequence( trackFilterRefit + LorentzAngleNtuple + hiteff + OfflineGainNtuple)
@@ -20,5 +23,5 @@ TkCalSeq_IsoMuon   = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + O
 TkCalSeq_IsoMuon0T = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_IsoMuon0T + hiteff)
 TkCalSeq_AagBunch   = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_AagBunch + hiteff)
 TkCalSeq_AagBunch0T = cms.Sequence(MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_AagBunch0T + hiteff)
-
+TkCalSeq_HitEff     = cms.Sequence(prescaleEvent + MeasurementTrackerEvent + trackFilterRefit + OfflineGainNtuple_StdBunch + hiteff)
 
