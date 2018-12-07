4147b4d1c7b159d2a47a10ee9d8651c25313e664
mmusich <marco.musich@cern.ch>
2018-12-07 12:03:26 +0100

    add the SiPixelFEDChannelQualityContainerWriteFromASCII payload producer

---
+++ CondFormats/SiPixelObjects/test/SiPixelFEDChannelQualityContainerWriteFromASCII.cc
@@ -0,0 +1,167 @@
+// -*- C++ -*-
+//
+// Package:    CondFormats/SiPixelObjects
+// Class:      SiPixelFEDChannelQualityContainerTestWriter
+// 
+/**\class SiPixelFEDChannelQualityContainerTestWriter SiPixelFEDChannelQualityContainerTestWriter.cc CondFormats/SiPixelObjects/plugins/SiPixelFEDChannelQualityContainerTestWriter.cc
+ Description: class to build the SiPixelFEDChannelQualityContainer payloads
+*/
+//
+// Original Author:  Marco Musich
+//         Created:  Wed, 27 Nov 2018 12:04:36 GMT
+//
+//
+
+// system include files
+#include <memory>
+#include <fstream>
+
+// user include files
+#include "CondCore/DBOutputService/interface/PoolDBOutputService.h"
+#include "CondFormats/SiPixelObjects/interface/SiPixelQuality.h"
+#include "CondFormats/SiPixelObjects/interface/SiPixelFEDChannelQualityContainer.h"
+#include "CondFormats/SiPixelObjects/interface/SiPixelFedCablingMap.h"
+#include "CondFormats/DataRecord/interface/SiPixelFedCablingMapRcd.h"
+#include "CondFormats/DataRecord/interface/SiPixelQualityFromDbRcd.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/ESWatcher.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+
+//
+// class declaration
+//
+
+class SiPixelFEDChannelQualityContainerWriteFromASCII : public edm::one::EDAnalyzer<>  {
+   public:
+      explicit SiPixelFEDChannelQualityContainerWriteFromASCII(const edm::ParameterSet&);
+      ~SiPixelFEDChannelQualityContainerWriteFromASCII();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+
+      // ----------member data ---------------------------
+      const std::string m_record;
+      const std::string m_SnapshotInputs;
+      const bool printdebug_;
+      SiPixelFEDChannelQualityContainer* myQualities;
+
+};
+
+//
+// constructors and destructor
+//
+SiPixelFEDChannelQualityContainerWriteFromASCII::SiPixelFEDChannelQualityContainerWriteFromASCII(const edm::ParameterSet& iConfig):
+  m_record(iConfig.getParameter<std::string>("record")),
+  m_SnapshotInputs(iConfig.getParameter<std::string>("snapshots")),
+  printdebug_(iConfig.getUntrackedParameter<bool>("printDebug",false))
+{
+  //now do what ever initialization is needed
+  myQualities = new SiPixelFEDChannelQualityContainer();
+}
+
+
+SiPixelFEDChannelQualityContainerWriteFromASCII::~SiPixelFEDChannelQualityContainerWriteFromASCII()
+{
+  delete myQualities;
+}
+
+//
+// member functions
+//
+
+// ------------ method called for each event  ------------
+void
+SiPixelFEDChannelQualityContainerWriteFromASCII::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+   using namespace edm;
+      
+   std::ifstream mysnapshots(m_SnapshotInputs); 
+   std::string  line; 
+
+   std::string scenario ="";
+   unsigned int thedetid(0);
+   
+   SiPixelFEDChannelQualityContainer::SiPixelFEDChannelCollection theBadFEDChannels;
+
+   if(mysnapshots.is_open()){
+     while (getline (mysnapshots, line)) {
+       //std::cout << line << std::endl;
+       std::istringstream iss (line);            
+       unsigned int run, ls, detid, fed, link, roc_first, roc_last;
+       iss >> run >> ls >> detid >> fed >> link >> roc_first >> roc_last;
+
+       PixelFEDChannel theBadChannel{fed,link,roc_first,roc_last};
+       
+       auto newscenario=std::to_string(run)+"_"+std::to_string(ls);
+       if(newscenario!=scenario){
+	 std::cout<< "================================" << std::endl;
+	 std::cout<< "found a new scenario: " << newscenario << std::endl;
+	 if(scenario!=""){
+	   std::cout<< "size of the fed channel vector: " <<  theBadFEDChannels.size() << std::endl;
+	   std::cout<< "================================" << std::endl;
+	   myQualities->setScenario(scenario,theBadFEDChannels);
+	   theBadFEDChannels.clear();
+	 }
+	 scenario=newscenario;
+       }
+
+       if(detid!=thedetid){
+	 if(printdebug_) std::cout<< "found a new detid!" << detid << std::endl;	 
+	 thedetid=detid;
+       } 
+       theBadFEDChannels[thedetid].push_back(theBadChannel);
+     }
+   }
+   
+   myQualities->setScenario(scenario,theBadFEDChannels);
+
+   if(printdebug_){
+     edm::LogInfo("SiPixelFEDChannelQualityContainerWriteFromASCII")<<"Content of SiPixelFEDChannelQualityContainer "<<std::endl;
+
+     // use buil-in method in the CondFormat
+     myQualities->printAll();
+   }
+}
+   
+// ------------ method called once each job just before starting event loop  ------------
+void 
+SiPixelFEDChannelQualityContainerWriteFromASCII::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+SiPixelFEDChannelQualityContainerWriteFromASCII::endJob() 
+{
+
+  edm::LogInfo("SiPixelFEDChannelQualityContainerWriteFromASCII")<<"Size of SiPixelFEDChannelQualityContainer object "<< myQualities->size() <<std::endl<<std::endl;
+
+  // Form the data here
+  edm::Service<cond::service::PoolDBOutputService> poolDbService;
+  if( poolDbService.isAvailable() ){
+    cond::Time_t valid_time = poolDbService->currentTime(); 
+    // this writes the payload to begin in current run defined in cfg
+    poolDbService->writeOne(myQualities,valid_time, m_record);
+  } 
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+SiPixelFEDChannelQualityContainerWriteFromASCII::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(SiPixelFEDChannelQualityContainerWriteFromASCII);
