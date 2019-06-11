08f3d7e8791c1965947cf246282c4d9b45a634d3
Henriette Aarup Petersen <henriette.aarup.petersen@cern.ch>
2019-06-11 17:26:47 +0200

    small bug fixes for primary Vertex, Zmumu and track split multi-iov validation

--- Alignment/OfflineValidation/python/TkAlAllInOneTool/primaryVertexValidationTemplates.py
+++ Alignment/OfflineValidation/python/TkAlAllInOneTool/primaryVertexValidationTemplates.py
@@ -164,8 +164,7 @@ process.p = cms.Path(process.goodvertexSkim*
 
 ####################################################################
 ####################################################################
-PVValidationScriptTemplate="""
-#!/bin/bash
+PVValidationScriptTemplate="""#!/bin/bash
 source /afs/cern.ch/cms/caf/setup.sh
 export X509_USER_PROXY=.oO[scriptsdir]Oo./.user_proxy
 
