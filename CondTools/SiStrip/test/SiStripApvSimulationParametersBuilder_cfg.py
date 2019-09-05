2145620d59c1aa58c687e90e7e85788f34d0f875
mmusich <marco.musich@cern.ch>
2019-09-05 18:48:57 +0200

    improve indentation

--- CondTools/SiStrip/test/SiStripApvSimulationParametersBuilder_cfg.py
+++ CondTools/SiStrip/test/SiStripApvSimulationParametersBuilder_cfg.py
@@ -51,21 +51,21 @@ process.PoolDBOutputService = cms.Service("PoolDBOutputService",
 
 
 process.WriteInDB = cms.EDAnalyzer("SiStripApvSimulationParametersBuilder",
-                                   apvBaselines_nBinsPerBaseline=cms.uint32(82),
-                                   apvBaselines_minBaseline=cms.double(0.),
-                                   apvBaselines_maxBaseline=cms.double(738.),
-                                   apvBaselines_puBinEdges=cms.vdouble(0.,  2.,  4.,  6.,  8., 10., 12., 14., 16., 18., 20., 22., 24., 26., 28., 30., 32., 34., 36., 38., 40., 42., 44., 46., 48., 50.),
-                                   apvBaselines_zBinEdges=cms.vdouble(0., 25., 50., 75.),
-                                   apvBaselinesFile_tib1=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TIB1_12us.txt"),
-                                   apvBaselinesFile_tib2=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TIB2_15us.txt"),
-                                   apvBaselinesFile_tib3=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TIB3_16us.txt"),
-                                   apvBaselinesFile_tib4=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TIB4_17us.txt"),
-                                   apvBaselinesFile_tob1=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TOB1_10us.txt"),
-                                   apvBaselinesFile_tob2=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TOB2_13us.txt"),
-                                   apvBaselinesFile_tob3=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TOB3_16us.txt"),
-                                   apvBaselinesFile_tob4=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TOB4_17us.txt"),
-                                   apvBaselinesFile_tob5=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TOB5_17us.txt"),
-                                   apvBaselinesFile_tob6=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TOB6_18us.txt")
-                                   )
+    apvBaselines_nBinsPerBaseline=cms.uint32(82),
+    apvBaselines_minBaseline=cms.double(0.),
+    apvBaselines_maxBaseline=cms.double(738.),
+    apvBaselines_puBinEdges=cms.vdouble(0.,  2.,  4.,  6.,  8., 10., 12., 14., 16., 18., 20., 22., 24., 26., 28., 30., 32., 34., 36., 38., 40., 42., 44., 46., 48., 50.),
+    apvBaselines_zBinEdges=cms.vdouble(0., 25., 50., 75.),
+    apvBaselinesFile_tib1=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TIB1_12us.txt"),
+    apvBaselinesFile_tib2=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TIB2_15us.txt"),
+    apvBaselinesFile_tib3=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TIB3_16us.txt"),
+    apvBaselinesFile_tib4=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TIB4_17us.txt"),
+    apvBaselinesFile_tob1=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TOB1_10us.txt"),
+    apvBaselinesFile_tob2=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TOB2_13us.txt"),
+    apvBaselinesFile_tob3=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TOB3_16us.txt"),
+    apvBaselinesFile_tob4=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TOB4_17us.txt"),
+    apvBaselinesFile_tob5=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TOB5_17us.txt"),
+    apvBaselinesFile_tob6=cms.FileInPath("SimTracker/SiStripDigitizer/data/APVBaselines_TOB6_18us.txt")
+)
 
 process.p = cms.Path(process.WriteInDB)
