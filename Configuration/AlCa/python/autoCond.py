9b25840393ac7256a574b7e779fb53ed70e5adcc
mmusich <marco.musich@cern.ch>
2020-05-28 17:07:15 +0200

    include GT without Phase-0 SiStrip records

--- Configuration/AlCa/python/autoCond.py
+++ Configuration/AlCa/python/autoCond.py
@@ -79,7 +79,7 @@ autoCond = {
     # GlobalTag for MC production with realistic conditions for Phase1 2024
     'phase1_2024_realistic'    : '111X_mcRun3_2024_realistic_v4', # GT containing realistic conditions for Phase1 2024
     # GlobalTag for MC production with realistic conditions for Phase2
-    'phase2_realistic'         : '110X_mcRun4_realistic_v3'
+    'phase2_realistic'         : '110X_mcRun4_realistic_noSiStrip_Candidate_2020_05_28_10_52_57'
 }
 
 aliases = {
