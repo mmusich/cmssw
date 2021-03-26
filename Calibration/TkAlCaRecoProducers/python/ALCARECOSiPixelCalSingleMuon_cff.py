5a6003281732e12a64eb5a3617207a9611a0e980
mmusich <marco.musich@cern.ch>
2021-03-26 11:55:58 +0100

    add prescale 100 to ALCARECOSiPixelCalSingleMuon

--- Calibration/TkAlCaRecoProducers/python/ALCARECOSiPixelCalSingleMuon_cff.py
+++ Calibration/TkAlCaRecoProducers/python/ALCARECOSiPixelCalSingleMuon_cff.py
@@ -9,6 +9,14 @@ ALCARECOSiPixelCalSingleMuonHLTFilter.andOr = True ## choose logical OR between 
 ALCARECOSiPixelCalSingleMuonHLTFilter.throw = False ## dont throw on unknown path names
 ALCARECOSiPixelCalSingleMuonHLTFilter.eventSetupPathsKey = 'SiPixelCalSingleMuon'
 
+# define the Prescaler service, and set the scale factors
+thPrescaleService = cms.Service('PrescaleService',
+                                prescaleTable = cms.VPSet(cms.PSet(pathName  = cms.string('pathALCARECOSiPixelCalSingleMuon'),
+                                                                   prescales = cms.vuint32( 100 )   # retain 1 event out of 100
+                                                               ))
+                               )
+scaler = cms.EDFilter('HLTPrescaler')
+
 import Alignment.CommonAlignmentProducer.AlignmentTrackSelector_cfi
 ALCARECOSiPixelCalSingleMuon = Alignment.CommonAlignmentProducer.AlignmentTrackSelector_cfi.AlignmentTrackSelector.clone()
 
@@ -19,4 +27,4 @@ ALCARECOSiPixelCalSingleMuon.etaMin = -3.5
 ALCARECOSiPixelCalSingleMuon.etaMax = 3.5
 
 # Sequence #
-seqALCARECOSiPixelCalSingleMuon = cms.Sequence(ALCARECOSiPixelCalSingleMuonHLTFilter+ALCARECOSiPixelCalSingleMuon)
+seqALCARECOSiPixelCalSingleMuon = cms.Sequence(ALCARECOSiPixelCalSingleMuonHLTFilter+scaler+ALCARECOSiPixelCalSingleMuon)
