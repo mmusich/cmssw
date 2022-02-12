0caf2d0bd3b74e013275d24c14807c1e60abba68
mmusich <marco.musich@cern.ch>
2022-02-12 09:26:32 +0100

    add additional test for output PPS AlCaReco processing

---
+++ Calibration/PPSAlCaRecoProducer/test/test_outputProducers.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+function die { echo $1: status $2; exit $2; }
+
+(cmsRun ${LOCAL_TEST_DIR}/test_express_PPSAlCaReco_output.py) || die 'failed running test_express_PPSAlCaReco_output.py' $?
+(cmsRun ${LOCAL_TEST_DIR}/test_prompt_PPSAlCaReco_output.py) || die 'failed running test_prompt_PPSAlCaReco_output.py' $?
