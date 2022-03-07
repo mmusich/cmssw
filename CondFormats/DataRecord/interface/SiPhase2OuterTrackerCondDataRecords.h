6556d67bf4aacb7070bb047849a64f26225ec216
mmusich <marco.musich@cern.ch>
2022-03-07 12:21:03 +0100

    add record for Phase-2 OT bad strips ESSource

--- CondFormats/DataRecord/interface/SiPhase2OuterTrackerCondDataRecords.h
+++ CondFormats/DataRecord/interface/SiPhase2OuterTrackerCondDataRecords.h
@@ -2,7 +2,9 @@
 #define CondFormats_SiPhase2OuterTrackerCondDataRecords_h
 
 #include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+#include "CondFormats/DataRecord/interface/SiStripCondDataRecords.h"  // needed for SiPhase2OuterTrackerBadStripRcd
 #include "Geometry/Records/interface/TrackerTopologyRcd.h"
+#include "Geometry/Records/interface/TrackerDigiGeometryRecord.h"
 #include "FWCore/Utilities/interface/mplVector.h"
 
 /*Record associated to SiPhase2OuterTrackerLorentzAngle Object: the SimRcd is used in simulation only*/
@@ -12,4 +14,7 @@ class SiPhase2OuterTrackerLorentzAngleRcd
 class SiPhase2OuterTrackerLorentzAngleSimRcd
     : public edm::eventsetup::DependentRecordImplementation<SiPhase2OuterTrackerLorentzAngleSimRcd,
                                                             edm::mpl::Vector<TrackerTopologyRcd> > {};
+class SiPhase2OuterTrackerBadStripRcd : public edm::eventsetup::DependentRecordImplementation<
+                                            SiStripBadStripRcd,
+                                            edm::mpl::Vector<TrackerTopologyRcd, TrackerDigiGeometryRecord> > {};
 #endif
