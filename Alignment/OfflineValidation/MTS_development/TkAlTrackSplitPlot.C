668a9f673d55bb652862965b8e5b3038295d556a
mmusich <marco.musich@cern.ch>
2024-01-02 15:17:33 +0100

    code-checks & code-format

--- Alignment/OfflineValidation/MTS_development/TkAlTrackSplitPlot.C
+++ Alignment/OfflineValidation/MTS_development/TkAlTrackSplitPlot.C
@@ -9,5 +9,11 @@ void TkAlTrackSplitPlot() {
   //fillmatrix();                                                         //(C)
   subdetector = "PIXEL";
 
-  makePlots("/afs/cern.ch/user/f/fvazzole/work/EPRs/tracker-alignment/newTool/CMSSW_12_4_12/src/Alignment/OfflineValidation/MTS_development/MTSValidation_PromptNewTemplate_1.0.root=Alignment in prompt with 400V pixel templates|1|2301," "/afs/cern.ch/user/f/fvazzole/work/EPRs/tracker-alignment/newTool/CMSSW_12_4_12/src/Alignment/OfflineValidation/MTS_development/MTSValidation_mp3619_1.0.root=mp3619|600|2001,", "/afs/cern.ch/user/f/fvazzole/work/EPRs/tracker-alignment/newTool/CMSSW_12_4_12/src/Alignment/OfflineValidation/MTS_development/TrackSplittingPlots");
+  makePlots(
+      "/afs/cern.ch/user/f/fvazzole/work/EPRs/tracker-alignment/newTool/CMSSW_12_4_12/src/Alignment/OfflineValidation/"
+      "MTS_development/MTSValidation_PromptNewTemplate_1.0.root=Alignment in prompt with 400V pixel templates|1|2301,"
+      "/afs/cern.ch/user/f/fvazzole/work/EPRs/tracker-alignment/newTool/CMSSW_12_4_12/src/Alignment/OfflineValidation/"
+      "MTS_development/MTSValidation_mp3619_1.0.root=mp3619|600|2001,",
+      "/afs/cern.ch/user/f/fvazzole/work/EPRs/tracker-alignment/newTool/CMSSW_12_4_12/src/Alignment/OfflineValidation/"
+      "MTS_development/TrackSplittingPlots");
 }
