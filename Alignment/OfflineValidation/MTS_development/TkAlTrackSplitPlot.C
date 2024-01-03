a1fdb353b4f11735395c0bc65f3470c949e7e812
mmusich <marco.musich@cern.ch>
2024-01-03 14:58:54 +0100

    update test of local plotting script

--- Alignment/OfflineValidation/MTS_development/TkAlTrackSplitPlot.C
+++ Alignment/OfflineValidation/MTS_development/TkAlTrackSplitPlot.C
@@ -8,7 +8,10 @@ void TkAlTrackSplitPlot() {
   //fillmatrix();                                                         //(C)
   subdetector = "PIXEL";
 
-  makePlots("/tmp/musich/CMSSW_14_0_X_2024-01-01-2300/src/Alignment/OfflineValidation/MTS_development/MTSValidation_PromptNewTemplate_1.0.root=Alignment in prompt with 400V pixel templates|1|2301,"
-	    "/tmp/musich/CMSSW_14_0_X_2024-01-01-2300/src/Alignment/OfflineValidation/MTS_development/MTSValidation_mp3619_1.0.root=mp3619|600|2001,",
-	    "/tmp/musich/CMSSW_14_0_X_2024-01-01-2300/src/Alignment/OfflineValidation/MTS_development/TrackSplittingPlots");
+  makePlots(
+      "/tmp/musich/CMSSW_14_0_X_2024-01-01-2300/src/Alignment/OfflineValidation/MTS_development/"
+      "MTSValidation_PromptNewTemplate_1.0.root=Alignment in prompt with 400V pixel templates|1|2301,"
+      "/tmp/musich/CMSSW_14_0_X_2024-01-01-2300/src/Alignment/OfflineValidation/MTS_development/"
+      "MTSValidation_mp3619_1.0.root=mp3619|600|2001,",
+      "/tmp/musich/CMSSW_14_0_X_2024-01-01-2300/src/Alignment/OfflineValidation/MTS_development/TrackSplittingPlots");
 }
