f475b660d3a6a9d411b5ff867f8460f6459961da
Marco Musich <marco.musich@cern.ch>
2025-04-01 22:19:20 +0200

    add customization function to comply with 2024 L1TMenu (L1Menu_Collisions2024_v1_3_0)

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -39,10 +39,35 @@ def customiseForOffline(process):
 
     return process
 
+def customiseFor2024L1TMenu(process):
+
+    seed_replacements = {
+        'L1_AXO_Medium' : 'L1_AXO_Nominal',
+        #disable new L1 seeds by renaming to a disabled seed.
+        'L1_SingleMu5_BMTF' : 'L1_SingleMuCosmics',
+        'L1_SingleMu13_SQ14_BMTF' : 'L1_SingleMuCosmics',
+        'L1_CICADA_VVVVTight' : 'L1_SingleMuCosmics',
+        'L1_CICADA_VVVTight' : 'L1_SingleMuCosmics',
+        'L1_CICADA_VVTight' : 'L1_SingleMuCosmics',
+        'L1_AXO_VVVTight' : 'L1_SingleMuCosmics',
+        'L1_AXO_VVTight' : 'L1_SingleMuCosmics'
+    }
+
+    for module in filters_by_type(process, 'HLTL1TSeed'):
+        l1Seed = module.L1SeedsLogicalExpression.value()
+        if any(old_seed in l1Seed for old_seed in seed_replacements):
+            # Replace each old seed with the new seed
+            for old_seed, new_seed in seed_replacements.items():
+                l1Seed = l1Seed.replace(old_seed, new_seed)
+            module.L1SeedsLogicalExpression = cms.string(l1Seed)
+
+    return process
+
 # CMSSW version specific customizations
 def customizeHLTforCMSSW(process, menuType="GRun"):
 
     process = customiseForOffline(process)
+    process = customiseFor2024L1TMenu(process)
 
     # add call to action function in proper order: newest last!
     # process = customiseFor12718(process)
