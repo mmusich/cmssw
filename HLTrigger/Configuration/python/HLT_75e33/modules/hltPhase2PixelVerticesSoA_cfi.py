e72d1d562e6e8c432451fa3343dd2a77859dbae0
Marco Musich <marco.musich@cern.ch>
2025-09-15 11:24:24 +0200

    alpaka pixel vertexing attempt

---
+++ HLTrigger/Configuration/python/HLT_75e33/modules/hltPhase2PixelVerticesSoA_cfi.py
@@ -0,0 +1,8 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoVertex.PixelVertexFinding.PixelVertexProducerAlpakaPhase2_alpaka import PixelVertexProducerAlpakaPhase2_alpaka as _PixelVertexProducerAlpakaPhase2_alpaka
+
+hltPhase2PixelVerticesSoA = _PixelVertexProducerAlpakaPhase2_alpaka(
+    PtMin = 1.0,
+    pixelTrackSrc = "hltPhase2PixelTracksSoA"
+)
