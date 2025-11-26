b2fcb449c28af04263c51e251dc07be82df64c20
Christian Wessel <christian.wessel@desy.de>
2025-11-26 15:51:32 +0100

    Introduce new simple seed creator and ElectronSeedFitter producers for egamma pixel seeding speedup.

---
+++ RecoTracker/TkSeedGenerator/plugins/SeedFromConsecutiveHitsCreatorNoFit.cc
@@ -0,0 +1,79 @@
+#include "FWCore/Framework/interface/ConsumesCollector.h"
+#include "FWCore/Framework/interface/FrameworkfwdMostUsed.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
+#include "FWCore/Utilities/interface/ESGetToken.h"
+#include "FWCore/Utilities/interface/ESInputTag.h"
+#include "FWCore/Utilities/interface/Likely.h"
+#include "FWCore/Utilities/interface/Visibility.h"
+
+#include "DataFormats/TrackingRecHit/interface/mayown_ptr.h"
+
+#include "RecoTracker/TkTrackingRegions/interface/TrackingRegion.h"
+#include "RecoTracker/TkSeedGenerator/interface/SeedCreator.h"
+#include "RecoTracker/TkSeedingLayers/interface/SeedComparitor.h"
+#include "RecoTracker/TkSeedingLayers/interface/SeedingHitSet.h"
+
+#include "TrackingTools/TrajectoryState/interface/TrajectoryStateTransform.h"
+#include "TrackingTools/TrajectoryState/interface/TrajectoryStateOnSurface.h"
+
+class dso_hidden SeedFromConsecutiveHitsCreatorNoFit : public SeedCreator {
+public:
+  SeedFromConsecutiveHitsCreatorNoFit(const edm::ParameterSet &, edm::ConsumesCollector &&);
+
+  ~SeedFromConsecutiveHitsCreatorNoFit() override = default;
+
+  static void fillDescriptions(edm::ParameterSetDescription &desc);
+  static const char *fillDescriptionsLabel() { return "ConsecutiveHitsSimple"; }
+
+  // initialize the "event dependent state"
+  void init(const TrackingRegion &region, const edm::EventSetup &es, const SeedComparitor *filter) final;
+
+  // make job
+  // fill seedCollection with the "TrajectorySeed"
+  void makeSeed(TrajectorySeedCollection &seedCollection, const SeedingHitSet &hits) final;
+
+protected:
+  const TrackingRegion *region = nullptr;
+  const SeedComparitor *filter = nullptr;
+};
+
+SeedFromConsecutiveHitsCreatorNoFit::SeedFromConsecutiveHitsCreatorNoFit(const edm::ParameterSet &cfg,
+                                                                           edm::ConsumesCollector &&iC) {}
+
+void SeedFromConsecutiveHitsCreatorNoFit::fillDescriptions(edm::ParameterSetDescription &desc) {}
+
+void SeedFromConsecutiveHitsCreatorNoFit::init(const TrackingRegion &iregion,
+                                                const edm::EventSetup &es,
+                                                const SeedComparitor *ifilter) {
+  region = &iregion;
+  filter = ifilter;
+}
+
+void SeedFromConsecutiveHitsCreatorNoFit::makeSeed(TrajectorySeedCollection &seedCollection,
+                                                    const SeedingHitSet &hits) {
+  if (hits.size() < 2)
+    return;
+
+  TrajectoryStateOnSurface state;
+  edm::OwnVector<TrackingRecHit> seedHits;
+
+  const TrackingRecHit *hit = nullptr;
+  for (unsigned int iHit = 0; iHit < hits.size(); iHit++) {
+    hit = hits[iHit]->hit();
+    if (!hit) {
+      continue;
+    }
+    seedHits.push_back(*hit);
+  }
+
+  PTrajectoryStateOnDet const &PTraj = trajectoryStateTransform::persistentState(state, hit->geographicalId().rawId());
+  seedCollection.emplace_back(PTraj, std::move(seedHits), alongMomentum);
+}
+
+#include "RecoTracker/TkSeedGenerator/interface/SeedCreatorFromRegionHitsEDProducerT.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+using NoFitSeedCreatorFromRegionConsecutiveHitsEDProducer =
+    SeedCreatorFromRegionHitsEDProducerT<SeedFromConsecutiveHitsCreatorNoFit>;
+DEFINE_FWK_MODULE(NoFitSeedCreatorFromRegionConsecutiveHitsEDProducer);
