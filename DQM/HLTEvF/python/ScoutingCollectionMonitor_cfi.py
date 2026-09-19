4781f1bdc375cafaf13820c3ee2b1c55537427b4
Marco Musich <marco.musich@cern.ch>
2026-09-19 21:14:59 +0200

    improve ScoutingCollectionMonitor

--- DQM/HLTEvF/python/ScoutingCollectionMonitor_cfi.py
+++ DQM/HLTEvF/python/ScoutingCollectionMonitor_cfi.py
@@ -34,7 +34,26 @@ scoutingCollectionMonitor = DQMEDAnalyzer('ScoutingCollectionMonitor',
                                           pfRecHitsEE            = cms.InputTag(""),
                                           pfCleanedRecHitsEB     = cms.InputTag(""),
                                           pfCleanedRecHitsEE     = cms.InputTag(""),
-                                          pfRecHitsHBHE          = cms.InputTag(""))
+                                          pfRecHitsHBHE          = cms.InputTag(""),
+                                          ## upper edges of the multiplicity histograms (Run 3 defaults);
+                                          ## override these to scale the plots for the Phase-2 occupancies
+                                          multiplicityRanges     = cms.PSet(
+                                              nTracks            = cms.int32(400),
+                                              nPrimaryVertices   = cms.int32(50),
+                                              nDisplacedVertices = cms.int32(10),
+                                              nMuons             = cms.int32(10),
+                                              nElectrons         = cms.int32(10),
+                                              nPhotons           = cms.int32(25),
+                                              nPFJets            = cms.int32(100),
+                                              nPFCands           = cms.int32(1000),
+                                              nEBRecHits         = cms.int32(1000),
+                                              nEERecHits         = cms.int32(1000),
+                                              nHBHERecHits       = cms.int32(2000),
+                                              pileUp             = cms.double(70.)),
+                                          rhoBinning             = cms.PSet(
+                                              nbins = cms.int32(100),
+                                              min   = cms.double(0.),
+                                              max   = cms.double(60.)))
 
 ## Add the scouting rechits monitoring (only for 2025, integrated in menu GRun 2025 V1.3)
 ## See https://its.cern.ch/jira/browse/CMSHLT-3607
@@ -46,3 +65,22 @@ from Configuration.Eras.Modifier_phase2_common_cff import phase2_common
                                               pfCleanedRecHitsEB = ("hltScoutingRecHitPacker", "EBCleaned"),
                                               pfCleanedRecHitsEE = ("hltScoutingRecHitPacker", "EECleaned"),
                                               pfRecHitsHBHE      = ("hltScoutingRecHitPacker", "HBHE"))
+
+phase2_common.toModify(
+    scoutingCollectionMonitor.multiplicityRanges,
+    pileUp            = 250,
+    nPhotons          = 40,
+    nPFJets           = 200,
+    nPFCands          = 5000,
+    nTracks           = 2000,
+    nPrimaryVertices  = 300,
+)
+
+phase2_common.toModify(
+    scoutingCollectionMonitor.rhoBinning,
+    nbins = 200,
+    min   = 0.,
+    max   = 200.
+)
+
+
